e9f0376773d6af19bf858274677797f27d0a4630
mohamednehad <zxreooo@gmail.com>
2022-09-04 19:26:06 -0400

    Code added, so we can requst the image and process it to filter function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,7 +30,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage',async(req: Request,res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url){
+      res.status(400).send("The URL provided for the image is needed");
+    }
+    const filtered_image=await filterImageFromURL(image_url);
+    res.status(200).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    })
+  });
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
